282786c640a3c259f964f54f532cce3afd8997a1
Anurag Singh <anusin2255@gmail.com>
2026-03-24 16:39:03 +0530

    Update database password in DBConnection.java

--- src/main/java/com/hospital/dao/DBConnection.java
+++ src/main/java/com/hospital/dao/DBConnection.java
@@ -6,7 +6,7 @@ import java.sql.DriverManager;
 public class DBConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/hospital_db";
     private static final String USER = "root";
-    private static final String PASSWORD = "15012007o";
+    private static final String PASSWORD = "passward";
 
     public static Connection getConnection() {
         Connection con = null;
